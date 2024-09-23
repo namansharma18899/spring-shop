76532705ff75419bbd6494ee0e3424b0ea173298
NamanSharmaPersonal <namansharma18899@gmail.com>
2024-09-23 08:22:44 +0530

    intial commit

---
+++ src/test/java/com/namanscode/spring_shop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namanscode.spring_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
